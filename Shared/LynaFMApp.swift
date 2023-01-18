8054ce5ec30afcb654eebdc4f231b5453a7f03c8
Alexander Panin <panin1970@gmail.com>
2023-01-18 17:29:42 +0300

    Initial Commit

---
+++ Shared/LynaFMApp.swift
@@ -0,0 +1,17 @@
+//
+//  LynaFMApp.swift
+//  Shared
+//
+//  Created by Александр Панин on 18.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct LynaFMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
